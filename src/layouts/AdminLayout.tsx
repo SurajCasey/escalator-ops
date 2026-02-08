dc4bc70147d766b546a5b9714255634207b058ec
Sandhya Timilsina Khatri <sandhyatimilsinakhatri@iMac.lan>
2026-02-08 14:47:17 +1100

    fixed error

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from "react-router-dom";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/Logo.png";
 import LogoutButton from "../components/LogoutButton";
 
 
